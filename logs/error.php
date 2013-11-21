930ddca563e90a07019852bcbdfc0d87e477e470
Jesús Cuenca <jesus.cuenca.lo@gmail.com>
2013-11-21 18:31:27 +0100

    Commit inicial.
    Añadidos todos los archivos de la aplicación Joomla

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-11-14 16:43:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-11-14T16:43:21+00:00	INFO	joomlafailure	El usuario y contraseña no coinciden o usted aún no tiene una cuenta.
